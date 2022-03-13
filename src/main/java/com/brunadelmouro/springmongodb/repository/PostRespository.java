31dbcf81db82d3eb27f38a27bfc25cdfae43592f
Bruna Delmouro <brunadelmouro@gmail.com>
2022-03-13 12:01:54 -0300

    Consulta simples com query methods

--- src/main/java/com/brunadelmouro/springmongodb/repository/PostRespository.java
+++ src/main/java/com/brunadelmouro/springmongodb/repository/PostRespository.java
@@ -5,6 +5,10 @@ import com.brunadelmouro.springmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRespository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/brunadelmouro/springmongodb/resources/PostResource.java
+++ src/main/java/com/brunadelmouro/springmongodb/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.brunadelmouro.springmongodb.resources;
 
 import com.brunadelmouro.springmongodb.domain.Post;
+import com.brunadelmouro.springmongodb.resources.util.URL;
 import com.brunadelmouro.springmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -21,5 +21,14 @@ public class PostResource {
         Post obj = postService.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+
+        List<Post> posts = postService.findByTitle(text);
+
+        return ResponseEntity.ok().body(posts);
     }
 }
